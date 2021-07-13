807d6ffb0520d4c8c148e2b41f19e2650dc05bf9
artem <artemza1tsev@hotmail.com>
2021-07-13 13:56:27 +0300

    created a class

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
+class NameGen { 
+    async loadUserData(user) {
+        const URL = 'https://api.github.com/';
+        const urls = [
+            `${URL}users/${user}`,
+        ];
+        
+        return fetch(urls)
+            .then(res => console.log(res.ok))
+    }
+}
+let nameGen = new NameGen
+console.log(nameGen.loadUserData("anton"))
 
- async function nameGen(str) {
-     let result = [] 
-     let resultFree = []  
-     const name = str.split(' ');   
-     const firstName = name[0].split('')  
-     const lastName = name[1].split('')
-     const firstLenght = firstName.join("").length
-     const lastLenght = lastName.join("").length
-     const lastFirst = Math.ceil((firstLenght + lastLenght) / 2 + 1)
-     for (let i = 1; i <= lastFirst; i++) {
-         const x = firstName.join('').substr(0, i) + lastName.join('').substr(0, i)
-         result.push(x)
-     }
-     for (let i = 0; i <= result.length; i++) {
-         const requestURL = "https://api.github.com/users/" + result[i]
-         const xhr = new XMLHttpRequest()
-         xhr.open('GET', requestURL, true)    
-         xhr.responseType = 'json'
-         xhr.onloadend = async () => {
-             if (xhr.status == 404) {
-              resultFree.push(result[i])
-              console.clear()
-              console.log(resultFree.sort(function(arg1, arg2) { return arg1.length - arg2.length })[0])                                                     
-             } 
-         }
-         xhr.send();
-     }
- }
- console.log(nameGen("alexey vorobiev"))
+
+
+
