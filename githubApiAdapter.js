433e2bea9f1d925269a01e65953b20ed836137d4
artem <artemza1tsev@hotmail.com>
2021-07-16 10:20:13 +0300

    fix

--- githubApiAdapter.js
+++ githubApiAdapter.js
@@ -2,7 +2,7 @@ const URL = 'https://api.github.com/';
 
 export default class githubApiAdapter {
     async getGithubStatus(username) {
-        const urlUser = `${URL}users/${username}?access_token=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`
+        const urlUser = `${URL}users/${username}`
         return fetch(urlUser)
             .then(res => res.status)
     }
